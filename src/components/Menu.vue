464aa09bdf3c4fbe791118dfba62a5c9411cb13d
Mohamed BEN YOUSSEF <moh.benysf@gmail.com>
2020-12-06 12:39:22 +0100

    create components

---
+++ src/components/Menu.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Home</a>
+    <div class="collapse navbar-collapse" id="navbarColor02">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Contact
+            <span class="sr-only">(current)</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+        </li>
+        </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+
+    export default {
+    name: 'Menu'
+    }
+</script>
+
+<style>
+</style>
